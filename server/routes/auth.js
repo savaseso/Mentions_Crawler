d797e1b4615051ef5d0e08829ea85dae89c87d1b
unknown <rigbence@gmail.com>
2021-01-26 10:30:01 -0800

    sending cookie to the client with JWT token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -12,12 +12,7 @@ router.post("/register", async (req, res, next) => {
       password,
     });
 
-    //create token
-    const token = user.getSignedJwtToken();
-
-    if (user) {
-      res.status(201).json({ message: "Registration was successful", token });
-    }
+    sendTokenResponse(user, 201, res, "Registration was successful");
 
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -27,32 +22,42 @@ router.post("/register", async (req, res, next) => {
 //login user
 router.post("/login", async (req, res, next) => {
   const { email, password } = req.body;
-  try{
-     //valid email or password
-  if (!email || !password) {
-    return res.status(401).json({ message: "invalid credentials" });
-  }
-  //check for user
-  const user = await User.findOne({ email: email }).select("+password");
-  if (!user) {
-    return res.status(401).json({ message: "invalid credentials" });
-  }
-  // check if password matches
-  const isMatch = await user.matchPassword(password);
+  try {
+    //valid email or password
+    if (!email || !password) {
+      return res.status(401).json({ message: "invalid credentials" });
+    }
+    //check for user
+    const user = await User.findOne({ email: email }).select("+password");
+    if (!user) {
+      return res.status(401).json({ message: "invalid credentials" });
+    }
+    // check if password matches
+    const isMatch = await user.matchPassword(password);
 
-  if (!isMatch) {
-    return res.status(401).json({ message: "invalid credentials" });
-  }
-  
-  const token = user.getSignedJwtToken();
+    if (!isMatch) {
+      return res.status(401).json({ message: "invalid credentials" });
+    }
 
-  res.status(201).json({ message: "Login was successful", token });
+    sendTokenResponse(user, 201, res, "Login was successful");
 
-  }
-  catch (err){
+  } catch (err) {
     res.status(400).json({ message: err.message });
   }
- 
 });
+
+//Get Token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res, message) => {
+  
+  const token = user.getSignedJwtToken();
+  const options = {
+    httpOnly: true,
+  };
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({ success: message, token });
+};
 
 module.exports = router;
